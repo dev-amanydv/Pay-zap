8ab725e7186b51399374c9193da1301a82e4131b
Aman Yadav <ay.work07@gmail.com>
2025-06-03 00:12:12 +0530

    Updated landing page

--- apps/user-app/components/ui/LandingPage.tsx
+++ apps/user-app/components/ui/LandingPage.tsx
@@ -1,9 +1,11 @@
-import { Timmana } from 'next/font/google'
+import { Poppins } from 'next/font/google'
+import Image from 'next/image'
 import React from 'react'
+import { MdArrowForwardIos} from 'react-icons/md'
 
-const timmana = Timmana({
+const poppins = Poppins({
     subsets: ['latin'],
-    weight: ['400']
+    weight: ['400', '500', '600', '700']
 })
 
 const LandingPage = () => {
@@ -11,8 +13,8 @@ const LandingPage = () => {
   return (
     <>
     <nav className='flex items-center text-md font-extralight justify-between text-white'>
-        <div className={`${timmana.className} text-4xl flex flex-col justify-center items-center font-bold text-blue-500`}>
-            <div className=''>PayZap</div>
+        <div className={`${poppins.className} text-4xl flex flex-col justify-center items-center font-semibold text-blue-600`}>
+            <div className='italic'>PayZap</div>
         </div>
         <div className='list-none flex space-x-10'>
             <li>Features</li>
@@ -31,16 +33,23 @@ const LandingPage = () => {
     <div>
         <section className="text-white py-24  w-full font-bold" >
             <div className='flex flex-col items-center'>
-                <div className='w-full flex justify-center pb-5 text-[36px] text-[#B8B8BC] items-center'>
-                    Ditch the cash. Just PayZap.
+                <div className='w-full text-center justify-center bg-gradient-to-r from-blue-800  to-gray-500 bg-clip-text text-transparent items-center pb-0 text-[24px] text-[#B8B8BC]'>
+                    <span>Ditch the cash. Just</span>  <span className={`${poppins.className} font-semibold italic text-[28px]`}> PayZap</span>
+                </div>
+                <div className='w-full flex bg-gradient-to-r from-blue-100 via-blue-300  to-blue-100 bg-clip-text text-transparent  justify-center gap-2 text-center pb-5 text-[42px] text-gray-100 items-center'>
+                    <span className='h-fit'>All-In-One <span className=''> Wallet</span> App,</span>
+                    <span className='text-[43px] h-fit  '>You Always Wanted.</span>
                 </div>
                 
-                <p className='text-lg text-center max-w-3xl font-light'>
-                    PayZap is your all-in-one payment and wallet solution for fast transfers, easy tracking, and total control.
-                    Skip the cash, live fast. Go digital with PayZap.
-                    Pay bills, transfer funds, and manage your wallet in one place.
-                    Fast, secure, and built for your daily needs.
+                <p className='text-lg text-center text-blue-100 opacity-85 max-w-2xl font-light'>
+                Manage your money effortlessly with our all-in-one wallet app. Send payments instantly, track balances, and stay in control.
                 </p>
+            </div>
+            <div className='flex justify-center pt-20'>
+                <button className='flex bg-purple-500 text-white py-2 px-4 rounded-full items-center font-semibold'>Get Started <span className='font-bold'><MdArrowForwardIos /></span></button>
+            </div>
+            <div className='w-full pt-20 flex pr-20 justify-center rounded-lg overflow-hidden items-center'>
+                <Image src='/hero3.svg' alt='hero' width={1000} height={1000} />
             </div>
             
         </section>
