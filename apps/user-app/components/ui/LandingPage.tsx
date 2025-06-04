dcb49600c27dd5b1f0f754e0272ae419d370757b
Aman Yadav <ay.work07@gmail.com>
2025-06-04 23:56:54 +0530

    Added feat

--- apps/user-app/components/ui/LandingPage.tsx
+++ apps/user-app/components/ui/LandingPage.tsx
@@ -34,7 +34,7 @@ const LandingPage = () => {
       <div>
         <section className="text-white py-24  w-full font-bold">
           <div className="flex flex-col items-center">
-            <div className="w-full text-center justify-center bg-gradient-to-r from-blue-800  to-gray-500 bg-clip-text text-transparent items-center pb-0 text-[24px] text-[#B8B8BC]">
+            <div className="w-full text-center justify-center bg-gradient-to-r from-blue-900 via-neutral-500  to-blue-950 bg-clip-text text-transparent items-center pb-0 text-[24px] text-[#B8B8BC]">
               <span>Ditch the cash. Just</span>{" "}
               <span
                 className={`${poppins.className} font-semibold italic text-[28px]`}
@@ -43,14 +43,14 @@ const LandingPage = () => {
                 PayZap
               </span>
             </div>
-            <div className="w-full flex bg-gradient-to-r from-blue-100 via-blue-300  to-blue-100 bg-clip-text text-transparent  justify-center gap-2 text-center pb-5 text-[42px] text-gray-100 items-center">
+            <div className="w-full flex bg-gradient-to-r from-neutral-300 via-neutral-400  to-blue-100 bg-clip-text text-transparent  justify-center gap-2 text-center pb-5 text-[42px] text-gray-100 items-center">
               <span className="h-fit">
                 All-In-One <span className=""> Wallet</span> App,
               </span>
               <span className="text-[43px] h-fit  ">You Always Wanted.</span>
             </div>
 
-            <p className="text-lg text-center text-blue-100 opacity-85 max-w-2xl font-light">
+            <p className="text-lg text-center text-neutral-400 opacity-85 max-w-2xl font-light">
               Manage your money effortlessly with our all-in-one wallet app.
               Send payments instantly, track balances, and stay in control.
             </p>
@@ -64,48 +64,49 @@ const LandingPage = () => {
             </button>
           </div>
           <div className="w-full relative pt-20 flex pr-20 justify-center rounded-lg overflow-hidden items-center">
-            <Image src="/hero3.svg" alt="hero" width={1000} height={1000} />
+            <Image src="/hero3.svg" className="opacity-70" alt="hero" width={1000} height={1000} />
           </div>
         </section>
         <section className="text-white p-8 w-full">
-          <div className="text-center my-10 w-fit mx-auto px-3 rounded-full text-md bg-slate-400">Features</div>
-          <div className="grid grid-cols-5 gap-4">
-            <div className=" bg-slate-800 relative flex justify-center overflow-hidden text-white shadow-lg rounded-3xl  col-span-3">
+          <div className="text-center my-10 w-fit mx-auto px-5 rounded-full text-md border-2 border-slate-400">Features</div>
+          <div className="grid grid-cols-5 gap-10">
+            <div className=" bg-slate-800 relative flex overflow-hidden text-white shadow-lg rounded-3xl  col-span-3">
                 <Image src="/circles.svg" className="absolute -top-16" alt="feat1" width={500} height={500} />
                 <Image src="/upi-qr.svg" className="absolute top-3 left-44" alt="feat1" width={500} height={500} />
                 <div className="absolute top-0 left-0 h-3/4 w-full bg-[linear-gradient(to_bottom,_rgba(0,0,0,0),_rgba(0,0,0,1))] pointer-events-none"></div>
                 <div className="absolute bottom-0 flex flex-col justify-center  left-0 h-1/4 w-full bg-black pointer-events-none">
-                <h1 className="text-4xl font-bold max-w-lg pl-5 bg-gradient-to-r from-blue-100 via-blue-200 to-blue-100 bg-clip-text text-transparent">Instant Money Transfer with UPI/QR</h1>
                 </div>
+                <h1 className="text-2xl w-96 absolute bottom-1/4 opacity-70 font-bold max-w-lg pl-5 bg-gradient-to-r from-neutral-100 via-neutral-200 to-neutral-100 bg-clip-text text-transparent">Instant Money Transfer with UPI/QR</h1>
+                <p className="text-sm w-3/4 text-neutral-500 absolute pl-6 bottom-9">Send and receive money instantly via UPI or by scanning any QR code — fast, secure, and hassle-free.</p>
             </div>
             <div className="bg-slate-800 overflow-hidden relative p-6 text-white shadow-lg rounded-3xl h-96 col-span-2">
                 <Image src={'/scribble.png'} className="absolute top-20 opacity-30 animate-bounce" alt="scribble" width={1000} height={1000} />
                 <Image src="/pay-bills.svg" className="absolute top-10 left-52" alt="feat1" width={250} height={100} />
                 <div className="absolute top-0 left-0 h-3/4 w-full bg-[linear-gradient(to_top,_rgba(0,0,0,1),_rgba(0,0,0,0))] pointer-events-none"></div> 
                 <div className="absolute bottom-0 left-0 h-1/4 w-full bg-black pointer-events-none">
-                <h1 className="text-4xl font-bold max-w-md pl-5 bg-gradient-to-r from-blue-100 via-blue-200 to-blue-100 bg-clip-text text-transparent">One Tap Bill Payments and Recharges</h1>
                 </div> 
+                <h1 className="text-2xl w-96 absolute bottom-1/4 opacity-70 font-bold max-w-lg bg-gradient-to-r from-neutral-100 via-neutral-200 to-neutral-100 bg-clip-text text-transparent">One Tap Bill Payments and Recharges</h1>
+                <p className="text-sm w-3/4 text-neutral-500 absolute  bottom-6">Send and receive money instantly via UPI or by scanning any QR code — fast, secure, and hassle-free.</p>
             </div>
             <div className="bg-slate-800 p-6 overflow-hidden relative text-white shadow-lg rounded-3xl h-96 col-span-2">
               <Image src={'/ring-square.png'} className="absolute -top-10 -left-1 opacity-50 animate-spin-slow" alt="ring-square" width={500} height={500} />
               <Image src={'/split-pay.svg'} className="absolute top-10 left-36 " alt="ring-square" width={2000} height={2000} />
-              <div className="absolute top-0 left-0 h-3/4 w-full bg-[linear-gradient(to_bottom,_rgba(0,0,0,0),_rgba(0,0,0,1))] pointer-events-none"></div>
-              <div className="absolute bottom-0 left-0 h-1/4 w-full bg-black pointer-events-none">
-              <h1 className="text-4xl font-bold max-w-md pl-5 bg-gradient-to-r from-blue-100 via-blue-200 to-blue-100 bg-clip-text text-transparent">
-                Split Payments and Expenses
-              </h1>
-              </div> 
+              <div className="absolute top-0 left-0 h-3/4 w-full bg-[linear-gradient(to_top,_rgba(0,0,0,1),_rgba(0,0,0,0))] pointer-events-none"></div> 
+                <div className="absolute bottom-0 left-0 h-1/4 w-full bg-black pointer-events-none">
+                </div> 
+                <h1 className="text-2xl w-96 absolute bottom-1/4 opacity-70 font-bold max-w-lg bg-gradient-to-r from-neutral-100 via-neutral-200 to-neutral-100 bg-clip-text text-transparent">Split Payments with Friends</h1>
+                <p className="text-sm w-3/4 text-neutral-500 absolute  bottom-6">Split restaurant bills, rent, or any group expense and track who paid what — perfect for students and roommates.
+                </p>
             </div>
             <div className="bg-slate-800 overflow-hidden p-6 relative text-white shadow-lg rounded-3xl h-96 col-span-3">
                <Image src={'/ring-element.png'} alt="ring-element" className="opacity-20 animate-spin-slow absolute -top-40 first-line:" width={1000} height={1000} />
                <Image src={'/wallet-pay.svg'} alt="ring-element" className=" absolute rotate-45 top-6 first-line" width={200} height={200} />
                <Image src={'/virtual-card.svg'} alt="ring-element" className=" absolute -rotate-45 left-[500px] -top-10 first-line:" width={250} height={250} />
-               <div className="absolute top-0 left-0 h-3/4 w-full bg-[linear-gradient(to_bottom,_rgba(0,0,0,0),_rgba(0,0,0,1))] pointer-events-none"></div>
-               <div className="absolute bottom-0 left-0 h-1/4 w-full bg-black pointer-events-none">
-               <h1 className="text-4xl font-bold max-w-md pl-5 bg-gradient-to-r from-blue-100 via-blue-200 to-blue-100 bg-clip-text text-transparent">
-                Virtual Cards for Online Payments
-               </h1>
-               </div>          
+               <div className="absolute top-0 left-0 h-3/4 w-full bg-[linear-gradient(to_top,_rgba(0,0,0,1),_rgba(0,0,0,0))] pointer-events-none"></div> 
+                <div className="absolute bottom-0 left-0 h-1/4 w-full bg-black pointer-events-none">
+                </div> 
+                <h1 className="text-2xl absolute bottom-1/4 opacity-70 font-bold w-96 bg-gradient-to-r from-neutral-100 via-neutral-200 to-neutral-100 bg-clip-text text-transparent">Virtual Cards for Online Payments</h1>       
+                <p className="text-sm w-3/4 text-neutral-500 absolute  bottom-9">Generate secure, app-linked virtual cards for safer online shopping without exposing your bank details.</p>
             </div>
           </div>
         </section>
